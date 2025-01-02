35989df1a4584a8fe0e0f5edb17d14b7135766b2
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-02 08:04:01 +0200

    Adding stack manipulation functionalities to the project

---
+++ ft_stk_man.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stk_man.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 13:15:39 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/12/31 14:42:36 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include "ft_stack.h"
+
+bool	stk_contain_duplicates(t_stack *stack)
+{
+	t_list	*node;
+	t_list	*start;
+	int		value;
+
+	node = stack->top;
+	while (node)
+	{
+		value = node->data;
+		start = node->next;
+		while (start)
+		{
+			if (value == start->data)
+				return (false);
+			start = start->next;
+		}
+		node = node->next;
+	}
+	return (true);
+}
+
+bool	stk_is_sorted(t_stack *stack, bool ascending)
+{
+	t_list	*current_node;
+	t_list	*next_node;
+
+	current_node = stack->top;
+	next_node = current_node->next;
+	while (current_node && next_node)
+	{
+		if (ascending)
+		{
+			if (current_node->data > next_node->data)
+				return (false);
+		}
+		else
+		{
+			if (current_node->data < next_node->data)
+				return (false);
+		}
+		current_node = next_node;
+		next_node = current_node->next;
+	}
+	return (true);
+}
