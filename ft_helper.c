bf90d5cd3f4db29f12f0f0425e068f3bdd530bc6
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-02 06:46:08 +0200

    Refactoring and update the code to meet norm BP

--- ft_helper.c
+++ ft_helper.c
@@ -22,47 +22,99 @@ bool	ft_isvalid(char *str)
 		count++;
 	if (str[count] == '-' || str[count] == '+')
 		count++;
-	while(str && str[count])
+	while (str && str[count])
 	{
-		if(!ft_isdigit(str[count]))
+		if (!ft_isdigit(str[count]))
 			return (false);
 		count++;
 	}
 	return (true);
 }
 
-bool	ft_handle_argument(int argc, char **argv, t_stack *stack)
+bool	ft_exit(char **ptr)
 {
-	char	**result;
-	int		count;
-	int		step;
+	int	count;
 
 	count = 0;
-	step = 1;
-	if(argc == 2)
+	while (ptr && ptr[count])
+	{
+		free(ptr[count]);
+		if (!ptr[count])
+			return (false);
+		count++;
+	}
+	free(ptr);
+	if (!ptr)
+		return (false);
+	return (true);
+}
+
+char	**ft_parse_argument(char **argv, int argc, int *start)
+{
+	char	**result;
+
+	if (argc == 2)
 	{
 		result = ft_split(argv[1], ' ');
-		while(result[count])
+		while (result && result[(*start)++])
 		{
-			if(!ft_isvalid(result[count]))
-				return (false);
-			count++;
+			if (!ft_isvalid(result[*start - 1]))
+			{
+				ft_exit(result);
+				return (NULL);
+			}
+		}
+		(*start)--;
+	}
+	else
+	{
+		while (++(*start) < argc)
+		{
+			if (!ft_isvalid(argv[*start]))
+				return (NULL);
+		}
+		result = argv;
+	}
+	return (result);
+}
+
+bool	ft_push_to_stack(char **arr, int argc, int count, t_stack *stack)
+{
+	if (argc == 2)
+	{
+		while (count >= 0)
+		{
+			if (!stk_push(stack, ft_atoi(arr[count])))
+				return (ft_exit(arr));
+			free(arr[count]);
+			count--;
 		}
 		return (true);
 	}
 	else
 	{
-		while(step < argc)
+		while (count > 0)
 		{
-			if(ft_isvalid(argv[step]))
-			{
-				stk_push(stack, ft_atoi(argv[step]));
-				ft_printf("added : %d\n", stk_peek(stack));
-			}
-			else
+			if (!stk_push(stack, ft_atoi(arr[count])))
 				return (false);
-			step++;
+			count--;
 		}
 		return (true);
 	}
+}
+
+bool	ft_handle_argument(int argc, char **argv, t_stack *stack)
+{
+	char	**result;
+	int		size;
+
+	size = 0;
+	result = ft_parse_argument(argv, argc, &size);
+	if (!result)
+		return (false);
+	if (!ft_push_to_stack(result, argc, (size - 1), stack))
+		return (false);
+	if (argc == 2)
+		free(result);
+	return (true);
 }
