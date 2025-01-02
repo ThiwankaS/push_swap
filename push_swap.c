e5ee1b3fbe6f6b968e19d61bf76fed8a1cdbdc15
Thiwanka Somachandra <thivankas@outlook.com>
2025-01-02 06:43:37 +0200

    Update the main function to print the stack

--- push_swap.c
+++ push_swap.c
@@ -1,14 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsomacha <tsomacha@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/31 13:15:39 by tsomacha          #+#    #+#             */
+/*   Updated: 2024/12/31 14:42:36 by tsomacha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_stack.h"
 #include "libft.h"
 
-int main(int argc, char *argv[])
+void	stk_print(t_stack *stack)
 {
-	t_stack stack;
+	t_list	*temp;
+
+	temp = stack->top;
+	while (temp)
+	{
+		ft_printf("item [%p] | data [%d]\n", temp, temp->data);
+		temp = temp->next;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_stack	stack;
 
 	stk_init(&stack);
-	if(argc > 1)
+	if (argc > 1)
 	{
-		if(!ft_handle_argument(argc, argv, &stack))
+		if (!ft_handle_argument(argc, argv, &stack))
 		{
 			ft_printf("Error!\n");
 			return (0);
@@ -16,8 +40,8 @@ int main(int argc, char *argv[])
 	}
 	else
 		ft_printf("Error!\n");
-
-	while(!stk_isempty(&stack))
+	stk_print(&stack);
+	while (!stk_isempty(&stack))
 		stk_pop(&stack);
 	return (0);
 }
