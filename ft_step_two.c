47ca6716a885f6d5bf1683319c24b548c2a12a92
Thiwanka Somachandra <thivankas@outlook.com>
2025-02-11 07:43:56 +0200

    Format and update to meet norm stds

--- ft_step_two.c
+++ ft_step_two.c
@@ -12,80 +12,73 @@
 
 #include "push_swap.h"
 
-int	two_step(t_stack *stack_a, t_stack *stack_b, t_list *guide)
+int	check_top_a(t_stack *stack_a, t_stack *stack_b, t_list *guide)
 {
-	t_list	*top_a;
-	t_list	*next_a;
-	t_list	*bottom_a;
 	int		top_b;
 	int		next_b;
 	int		bottom_b;
+	t_list	*top_a;
 
+	top_b = ft_stk_peek(stack_b);
+	next_b = ft_stk_get_next(stack_b);
+	bottom_b = ft_stk_get_bottom(stack_b);
 	top_a = ft_list_get_node(guide, ft_stk_peek(stack_a));
-	next_a = ft_list_get_node(guide, ft_stk_get_next(stack_a));
+	if (top_a->next->data == top_b)
+		return (ra(stack_a), 1);
+	else if (top_a->prev->data == next_b)
+		return (rb(stack_b), 1);
+	else if (top_a->prev->data == bottom_b)
+		return (rrb(stack_b), 1);
+	else if (top_a->next->data == next_b)
+		return (rr(stack_a, stack_b), 1);
+	return (0);
+}
+
+int	check_bottom_a(t_stack *stack_a, t_stack *stack_b, t_list *guide)
+{
+	int		top_b;
+	int		next_b;
+	int		bottom_b;
+	t_list	*bottom_a;
+
 	bottom_a = ft_list_get_node(guide, ft_stk_get_bottom(stack_a));
 	top_b = ft_stk_peek(stack_b);
 	next_b = ft_stk_get_next(stack_b);
 	bottom_b = ft_stk_get_bottom(stack_b);
-	if (top_a->next->data == top_b)
-	{
-		ra(stack_a);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (next_a->prev->data == top_b)
-	{
-		ra(stack_a);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (top_a->prev->data == next_b)
-	{
-		rb(stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (top_a->next->data == next_b)
-	{
-		rr(stack_a, stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (top_a->prev->data == bottom_b)
-	{
-		rrb(stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (next_a->prev->data == next_b)
-	{
-		rr(stack_a, stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (bottom_a->prev->data == top_b)
-	{
-		rra(stack_a);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (bottom_a->next->data == next_b)
-	{
-		rb(stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
-	else if (bottom_a->prev->data == bottom_b)
-	{
-		rrr(stack_a, stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
+	if (bottom_a->next->data == next_b)
+		return (rb(stack_b), 1);
 	else if (bottom_a->next->data == bottom_b)
-	{
-		rrb(stack_b);
-		pa(stack_a, stack_b);
-		return (1);
-	}
+		return (rrb(stack_b), 1);
+	else if (bottom_a->prev->data == top_b)
+		return (rra(stack_a), 1);
+	else if (bottom_a->prev->data == bottom_b)
+		return (rrr(stack_a, stack_b), 1);
+	return (0);
+}
+
+int	check_next_a(t_stack *stack_a, t_stack *stack_b, t_list *guide)
+{
+	int		top_b;
+	int		next_b;
+	t_list	*next_a;
+
+	next_a = ft_list_get_node(guide, ft_stk_get_next(stack_a));
+	top_b = ft_stk_peek(stack_b);
+	next_b = ft_stk_get_next(stack_b);
+	if (next_a->prev->data == top_b)
+		return (ra(stack_a), 1);
+	else if (next_a->prev->data == next_b)
+		return (rr(stack_a, stack_b), 1);
+	return (0);
+}
+
+int	two_step(t_stack *stack_a, t_stack *stack_b, t_list *guide)
+{
+	if (check_top_a(stack_a, stack_b, guide))
+		return (pa(stack_a, stack_b), 1);
+	if (check_bottom_a(stack_a, stack_b, guide))
+		return (pa(stack_a, stack_b), 1);
+	if (check_next_a(stack_a, stack_b, guide))
+		return (pa(stack_a, stack_b));
 	return (0);
 }
